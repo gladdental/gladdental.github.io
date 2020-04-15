453dde6852c5117c66f312101aed4d60c125fbc1
baphuc1998 <baphucb1605354@gmail.com>
2020-04-15 11:12:28 +0700

    MESSAGE

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  urlApi: 'https://baphuc10081998.pythonanywhere.com/',
-  // token_admin : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTc0MDgyMTgzLCJqdGkiOiJjMzU4NTUwODZiNTQ0OTdjYTMzMmJiMTlmNTRiMDgxYiIsInVzZXJfaWQiOjEsInR5cGUiOiJhZG1pbiJ9.qKMOtTYzUzBfNoUaGAK6ray9IorBtsmgTl6cPREh2q0"
-  token: localStorage.getItem('bearToken')
-};
