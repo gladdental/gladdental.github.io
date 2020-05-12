be1d794c6be999097775832e72dbb1feacba9fd7
baphuc1998 <darknessnbp@gmail.com>
2020-05-12 14:26:24 +0700

    MESSAGE

--- src/app/models/customer.class.ts
+++
@@ -1,6 +0,0 @@
-export class detailuser{
-	public id: number;
-    public name: string;
-    public phone: string;
-    public qrcoce: string;
-}
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  // urlApi: 'http://127.0.0.1:8000',
-  urlApi: 'https://baphuc10081998.pythonanywhere.com/',
-  token: localStorage.getItem('bearToken')
-};
